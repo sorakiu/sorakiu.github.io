2094ad0d452ed3514bc1caa6baadfe728a0e923c
sorakiu <sorakiu@gmail.com>
2015-04-10 19:19:05 -0400

    rough draft of better layout

---
+++ _plugins/active_page.rb
@@ -0,0 +1,16 @@
+module MidgetonToes
+  class ActivePageTag < Liquid::Tag
+      def initialize(tag_name, text, tokens)
+          super
+          @text = text
+      end
+
+      def render(context)
+          name = context.environments.first["page"]["name"]
+          return '' if name.nil? or @text.nil?
+          return name == @text ? ' active ' : ''
+      end
+  end
+end
+
+Liquid::Template.register_tag('active_page', MidgetonToes::ActivePageTag)
